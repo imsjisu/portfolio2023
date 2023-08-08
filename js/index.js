51cb443202ce0c7df457eef082e43014e6e8ae09
jisu <132331661+imsjisu@users.noreply.github.com>
2023-08-09 08:53:19 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,13 +21,12 @@ function wheel (element){
   }
 }
 
+// 브랜드 이미지
 let innerTitleLine = document.querySelector("div.line");
 let danggn = document.querySelector(".danggn");
 let hermes = document.querySelector(".hermes");
 let binggrae = document.querySelector(".binggrae");
 let ralph = document.querySelector(".ralph");
-
-let html = document.querySelector(".page5_inner>.keywords>span:first-of-type");
 
 document.addEventListener("scroll", () => {
   let scroll = window.scrollY;
@@ -39,12 +38,44 @@ document.addEventListener("scroll", () => {
     danggn.classList.add("on");
   } else if(scroll >= 1700 && scroll < 2100){
     hermes.classList.add("on");
-  } else if(scroll >= 2100 && scroll < 2674){
+  } else if(scroll >= 2100 && scroll < 2500){
     binggrae.classList.add("on");
-  } else if(scroll >= 2674){
+  } else if(scroll >= 2500){
     ralph.classList.add("on");
-  } else if(scroll >=3400) {
-    html.classList.add("on");
   }
+  // else if(scroll >=3400) {
+  //   html.classList.add("on");
+  // }
 })
 
+
+// 사이트 컨텐츠 클릭 이벤트
+let danggnImg = document.querySelector(".danggn_inner");
+let hermesImg = document.querySelector(".hermes_inner");
+let binggraeImg = document.querySelector(".binggrae_inner");
+let ralphImg = document.querySelector(".ralph_inner");
+let projectTxt = document.querySelectorAll(".project_txt"); // 설명 섹션 부분
+console.log(projectTxt);
+
+danggnImg.addEventListener("click", (e) => {
+  danggnImg.classList.add("color");
+  projectTxt[0].classList.add("on");
+})
+
+hermesImg.addEventListener("click", (e) => {
+  hermesImg.classList.add("color");
+  projectTxt[1].classList.add("on");
+})
+
+binggraeImg.addEventListener("click", (e) => {
+  binggraeImg.classList.add("color");
+  projectTxt[2].classList.add("on");
+})
+
+ralphImg.addEventListener("click", (e) => {
+  ralphImg.classList.add("color");
+  projectTxt[3].classList.add("on");
+})
+
+
+// let html = document.querySelector(".page5_inner>.keywords>span:first-of-type");
