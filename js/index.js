71bed735fcbb4dc2fba7f0fc730015a0dd46afd0
jisu <132331661+imsjisu@users.noreply.github.com>
2023-08-09 17:50:59 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,47 @@ window.onload = function() {
 }
 
 
+// 2페이지 텍스트 opacity 변경
+let opacityTxt1 = document.querySelector(".page2>p.txt>span:first-of-type");
+let opacityTxt2 = document.querySelector(".page2>p.txt>span:nth-of-type(2)");
+let opacityTxt3 = document.querySelector(".page2>p.txt>span:nth-of-type(3)");
+let opacityTxt4 = document.querySelector(".page2>p.txt>span:nth-of-type(4)");
+let opacityTxt5 = document.querySelector(".page2>p.txt>span:nth-of-type(5)");
+let opacityTxt6 = document.querySelector(".page2>p.txt>span:nth-of-type(6)");
+let opacityTxt7 = document.querySelector(".page2>p.txt>span:nth-of-type(7)");
+let opacityTxt8 = document.querySelector(".page2>p.txt>span:nth-of-type(8)");
+let opacityTxt9 = document.querySelector(".page2>p.txt>span:nth-of-type(9)");
+let opacityTxt10 = document.querySelector(".page2>p.txt>span:last-of-type");
+console.log(opacityTxt9);
+
+document.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  console.log(scroll)
+
+  if(scroll >= 300 && scroll < 360) {
+    opacityTxt1.classList.add("on");
+  } else if(scroll >= 360 && scroll < 420){
+    opacityTxt2.classList.add("on");
+  } else if(scroll >= 420 && scroll < 480){
+    opacityTxt3.classList.add("on");
+  } else if(scroll >= 480 && scroll < 540){
+    opacityTxt4.classList.add("on");
+  } else if(scroll >= 540 && scroll < 600){
+    opacityTxt5.classList.add("on");
+  } else if(scroll >= 600 && scroll < 660){
+    opacityTxt6.classList.add("on");
+  } else if(scroll >= 660 && scroll < 720){
+    opacityTxt7.classList.add("on");
+  } else if(scroll >= 720 && scroll < 780){
+    opacityTxt8.classList.add("on");
+  } else if(scroll >= 780 && scroll < 840){
+    opacityTxt9.classList.add("on");
+  } else if(scroll >= 840){
+    opacityTxt10.classList.add("on");
+  }
+})
+
+
 // 사이트 컨텐츠 휠이벤트
 function wheel (element){
   for(let i=0; i<siteTrain.length; i++){
@@ -30,7 +71,7 @@ let ralph = document.querySelector(".ralph");
 
 document.addEventListener("scroll", () => {
   let scroll = window.scrollY;
-  console.log(scroll)
+  // console.log(scroll)
 
   if(scroll >= 1180 && scroll < 1300) {
     innerTitleLine.classList.add("on");
@@ -55,7 +96,7 @@ let hermesImg = document.querySelector(".hermes_inner");
 let binggraeImg = document.querySelector(".binggrae_inner");
 let ralphImg = document.querySelector(".ralph_inner");
 let projectTxt = document.querySelectorAll(".project_txt"); // 설명 섹션 부분
-console.log(projectTxt);
+// console.log(projectTxt);
 
 danggnImg.addEventListener("click", (e) => {
   danggnImg.classList.add("color");
